2eed4792229efc1424f12edba317faefed36c73f
AJP <arvind.puvi@gmail.com>
2018-10-16 15:51:57 +1100

    Updating User schema to include polls

---
+++ models/Poll.js
@@ -0,0 +1,11 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const pollSchema = new Schema({
+//   title: String,
+//   options: {}
+// });
+
+// const Poll = mongoose.model("poll", pollSchema);
+
+// module.exports = Poll;
--- models/User.js
+++ models/User.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
+// const Poll = require("./Poll");
 const Schema = mongoose.Schema;
+
+const pollSchema = new Schema({
+  title: String,
+  options: [
+    {
+      option: String
+    }
+  ]
+});
 
 const userSchema = new Schema({
   username: String,
-  googleId: String
+  googleId: String,
+  userPolls: [pollSchema]
 });
 
 const User = mongoose.model("user", userSchema);
