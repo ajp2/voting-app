cab1c58f65c3938f134216abf2ec335437bf1588
AJP <arvind.puvi@gmail.com>
2018-10-22 23:04:21 +1100

    updating

--- controllers/controller.poll.js
+++ controllers/controller.poll.js
@@ -2,22 +2,22 @@ const Poll = require("../models/Poll");
 
 exports.home = (req, res) => {
   res.redirect("/polls");
-}
+};
 
 exports.displayPolls = async (req, res) => {
   const allPolls = await Poll.find({}, "id title").sort({ createdAt: -1 });
   res.render("home", { user: req.user, allPolls: allPolls });
-}
+};
 
 exports.displayOne = async (req, res) => {
   const currentPoll = await Poll.findById(req.params.pollId);
   res.render("poll", { user: req.user, currentPoll: currentPoll });
-}
+};
 
 exports.deletePoll = async (req, res) => {
   await Poll.findByIdAndDelete(req.params.pollId);
   res.redirect("/polls");
-}
+};
 
 exports.myPolls = async (req, res) => {
   // find polls that have the same user id as the current user and sort by most recent
@@ -25,7 +25,7 @@ exports.myPolls = async (req, res) => {
     createdAt: -1
   });
   res.render("myPolls", { user: req.user, polls: polls });
-}
+};
 
 exports.create = (req, res) => {
   res.render("createPoll", { user: req.user });
@@ -52,7 +52,7 @@ exports.createPoll = (req, res) => {
   Poll.create(poll).then(poll => {
     res.redirect("/polls/" + poll.id);
   });
-}
+};
 
 exports.votePoll = async (req, res) => {
   // if user chooses to add another option, add it to the db
@@ -77,15 +77,16 @@ exports.votePoll = async (req, res) => {
   }
 
   // give the chart sometime to load
-  await setTimeout(() => { }, 50);
+  await setTimeout(() => {}, 50);
 
   res.redirect("/polls/" + req.params.pollId);
-}
+};
 
+// if user is logged in, continue to next middleware. Otherwise redirect to login page
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
   } else {
     res.redirect("/auth/google");
   }
-}
+};
